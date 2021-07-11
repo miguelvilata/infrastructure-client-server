a220799d111cfe77feec283e0c9f73551968d063
Reinaldo León <reinaldo.leon@smartprotection.com>
2021-07-11 14:07:56 +0200

    Test action fix eip attach

--- ec2/eip.tf
+++ ec2/eip.tf
@@ -1,10 +1,10 @@
 ################################################
 ## CREATE EIP AND ASSOCIATED WITH AN INSTANCE ##
 ################################################
-resource "aws_eip" "eip_bastion" {
+resource "aws_eip" "eip" {
   vpc      = true
 }
 resource "aws_eip_association" "eip_assoc_bastion" {
-  instance_id   = module.ec2_cluster.id[0]
-  allocation_id = aws_eip.eip_bastion.id
+  instance_id   = aws_instance.app.id
+  allocation_id = aws_eip.eip.id
 }
