97bc043d2424524210250f87991af4afe593afa7
Reinaldo León <reinaldo.leon@smartprotection.com>
2021-07-11 13:59:00 +0200

    Test action client server demo

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,14 +1,14 @@
-output "jenkins_id" {
-  value = aws_instance.jenkins.id
+output "id" {
+  value = aws_instance.app.id
 }
-output "jenkins_public_ip" {
-  value = aws_eip.eip_jenkins.id
+output "public_ip" {
+  value = aws_eip.eip_app.id
 }
 output "dns_cname" {
-  value = aws_route53_record.jenkins.name
+  value = aws_route53_record.app.name
 }
 
-output "jenkins_sg" {
-  value = aws_security_group.jenkins.name
+output "sg" {
+  value = aws_security_group.app.name
 }
 
